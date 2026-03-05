7ad9241c047a6328ec02d95a4671067dc563146c
NRMT-MAIN <sahunirmit.main@gmail.com>
2026-03-05 10:50:09 +0530

    Buffered and Unbuffered Channel

--- Go-Advance/channels.go
+++ Go-Advance/channels.go
@@ -6,21 +6,90 @@ import (
 )
 
 func channelsExample() {
-	greetings := make(chan string)
-	greetString := "Hello, Channel!"
+	// greetings := make(chan string)
+	// greetString := "Hello, Channel!"
+
+	// go func() {
+	// 	greetings <- greetString
+	// 	greetings <- "Welcome to Go programming!"
+	// }()
+
+	// time.Sleep(5 * time.Second) // Unbuffer channel will block until the value is received
+	// go func() {
+	// 	reciever := <-greetings
+	// 	println(reciever)
+	// 	reciever = <-greetings
+	// 	println(reciever)
+	// }()
+
+	// time.Sleep(1 * time.Second)
+	// fmt.Println("End of the Program")
+
+	// ch := make(chan int)
+	// go func() {
+	// 	ch <- 42
+	// 	time.Sleep(2 * time.Second) // Simulate some work before sending the value
+	// 	fmt.Println("2 second Goroutine finished")
+	// }()
+
+	// go func ()  {
+	// 	time.Sleep(3 * time.Second)
+	// 	fmt.Println("3 second Goroutine finished")
+	// }()
+
+	// reciever := <-ch 
+	// fmt.Println("Received value:", reciever)
+	// fmt.Println("End of the Program")
+
+	// Unbuffered channel example
+	// ch := make(chan int , 3) // Buffered channel with capacity of 3
+
+	// go func() {
+	// 	ch <- 1
+	// 	fmt.Println("Sent 1")
+	// 	ch <- 2
+	// 	fmt.Println("Sent 2")
+	// 	ch <- 3
+	// 	fmt.Println("Sent 3")
+	// }()
+
+	// time.Sleep(1 * time.Second) // Simulate some work before receiving values
+	// //ch <- 4 // This will block until there is space in the channel
+	// //fmt.Println("Sent 4")
+	// fmt.Println("Value: " , <-ch)
+	// fmt.Println("Value: " , <-ch)
+	// ch <- 4 // This will block until there is space in the channel
+	// fmt.Println("Sent 4")
+	// fmt.Println("Value: " , <-ch)
+
+
+	// time.Sleep(1 * time.Second) // Wait for all goroutines to finish
+	// fmt.Println("End of the Program")
+	// ch := make(chan int, 2)
+	// ch <- 1
+	// ch <- 2
+	// go func() {
+	// 	time.Sleep(2 * time.Second)
+	// 	fmt.Println("Received:", <- ch)
+	// }()
+	// fmt.Println("Blocking starts!")
+	// ch <- 3
+	// fmt.Println("Blocking ends!")
+	// fmt.Println("Received:", <- ch)
+	// fmt.Println("Received:", <- ch)
+
+	// fmt.Println("Buffered Channels")
+
+	// Blocking on recieve only if the buffer is empty
+	ch := make(chan int, 2)
 
 	go func() {
-		greetings <- greetString
-		greetings <- "Welcome to Go programming!"
+		time.Sleep(2 * time.Second)
+		ch <- 1
+		ch <- 2
 	}()
 
-	go func() {
-		reciever := <-greetings
-		println(reciever)
-		reciever = <-greetings
-		println(reciever)
-	}()
-
-	time.Sleep(1 * time.Second)
-	fmt.Println("End of the Program")
+	fmt.Println("Value : ", <-ch)
+	fmt.Println("Value : ", <-ch)
+	fmt.Println("End of the program")
 }
